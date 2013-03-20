765f890e0894933ea0b2508c5c6c8a128135eec5
drmaas <drmaas@gmail.com>
2013-03-19 22:28:12 -0500

    add a todo

--- orangutan/lab1/timer_loop_test/test.c
+++ orangutan/lab1/timer_loop_test/test.c
@@ -16,9 +16,10 @@ int main()
   volatile int i,j;
 
   // Measure time for loop execution (without test code)
-  long iteration = 20000;
+  long iteration = 10000;
   long test_iteration = 1000;
 
+  //TODO: possible only call ticks_to_microseceonds once after all times are recorded.
   //time to loop million times
   long time1 = ticks_to_microseconds(get_ticks());
   for (i=0; i < iteration; i++);
@@ -32,12 +33,16 @@ int main()
   long time4 = ticks_to_microseconds(get_ticks());
   long loop2time = time4 - time3;
 
-  //calculate time to loop 1 time in microseconds
-  //result is about 9.243 ms
-  long wcet = (loop2time - loop1time)/test_iteration;
+  //calculate time to loop 1000 times in microseconds
+  //result is about .924 ms
+  //so 1082 iterations take 1 ms
+  //for some reason this is actually 2 ms, so divide by 2
+  long wcet = (loop2time - loop1time)/iteration;
+  long one_ms = (1000*test_iteration)/(wcet*2); 
 
   clear();
   lcd_goto_xy(0,0);
-  lcd_goto_xy(0,0);
   printf("time:%li",wcet);
+  lcd_goto_xy(0,1);
+  printf("1 ms iter:%li",one_ms);
 }
